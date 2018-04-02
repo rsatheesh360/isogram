3ff62734f2633fe169e81d07e5a09ce58c3b170f
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-04-02 13:26:07 +0530

    Update and rename .gitignore to isogram.py

---
+++ .gitignore/isogram.py
@@ -0,0 +1,16 @@
+def isogram():
+	s=input()
+	l=list(s)
+	r=[]
+	for i in l:
+		if not i in r:
+			r.append(i)
+	if len(l)==len(r):
+		print('yes')
+	else :
+		print('no')
+try:
+	isogram()
+except:
+	print('invalid')
+  print(n)
